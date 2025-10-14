39f288d83aafdd6eff92795670f9fb55369fdc5f
Jedrzej <jpt.graczykowski@gmail.com>
2025-10-14 17:10:58 +0200

    nj-48 fix the problem with tag checkboxes on the new note form

--- app/models/notes_tag.rb
+++ app/models/notes_tag.rb
@@ -4,4 +4,12 @@ class NotesTag < ApplicationRecord
   belongs_to :user
 
   validates :note_id, uniqueness: { scope: :tag_id }
+
+  before_validation :assign_user
+
+  private
+
+  def assign_user
+    self.user_id ||= tag.user_id
+  end
 end
